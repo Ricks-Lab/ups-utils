040ce2fcff55573ded269b3472e55516a9df35fc
Rick <ricks.homelab@gmail.com>
2022-07-04 20:26:11 +0800

    improved __str__ format and grammar

--- UPSmodules/UPSmodule.py
+++ UPSmodules/UPSmodule.py
@@ -644,11 +644,15 @@ class UpsList:
 
     def __str__(self) -> str:
         num_ups = self.num_upss()
-        out_str = '{} UPSs listed in {}.\n'.format(num_ups['total'], env.UT_CONST.ups_json_file)
-        out_str += '    {} are compatible, {} are accessible, {} are responsive, '\
-                   '{} are valid, and {} are daemon.\n'.format(num_ups['compatible'], num_ups['accessible'],
-                                                               num_ups['responsive'], num_ups['valid'],
-                                                               num_ups['daemon'])
+        out_str = '{} {} listed in {}.\n'.format(num_ups['total'], 'UPS' if num_ups['total'] == 1 else 'UPSs',
+                                                 env.UT_CONST.ups_json_file)
+        out_str += '    {} {} compatible, {} {} accessible, {} {} responsive, '\
+                   '{} {} valid, and {} {} daemon.\n'.format(
+                       num_ups['compatible'], 'are' if num_ups['compatible'] != 1 else 'is',
+                       num_ups['accessible'], 'are' if num_ups['accessible'] != 1 else 'is',
+                       num_ups['responsive'], 'are' if num_ups['responsive'] != 1 else 'is',
+                       num_ups['valid'], 'are' if num_ups['valid'] != 1 else 'is',
+                       num_ups['daemon'], 'are' if num_ups['daemon'] != 1 else 'is')
         return out_str
 
     def __getitem__(self, uuid: str) -> UpsItem:
